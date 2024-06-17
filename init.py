2239ff5d76e581ee54be62d2536f0ee3b6901612
Israel-Kindudi <kindudimandiangu@gmail.com>
2024-06-17 14:54:51 +0100

    price and descriptions changes

--- init.py
+++ init.py
@@ -6,12 +6,12 @@ with app.app_context():
     db.create_all()
     if not PigType.query.all():
         pig_types = [
-            PigType(name="Piétrain", description="A popular pig breed", price=38.0),
-            PigType(name="Valens", description="Known for large litters", price=54.0),
-            PigType(name="Duroc", description="Great growth rate", price=54.0),
-            PigType(name="Landracs", description="High-quality meat", price=82.0),
-            PigType(name="Large White", description="Robust and healthy", price=82.0),
-            PigType(name="Thor", description="Muscular build", price=38.0)
+            PigType(name="Piétrain", description="éfficacité Alimentaire", price=38.0),
+            PigType(name="Valens", description="Rentabilité", price=54.0),
+            PigType(name="Duroc", description="Croissance", price=54.0),
+            PigType(name="Landracs", description="Capacité à sevrer", price=82.0),
+            PigType(name="Large White", description="qualité du porcelet", price=82.0),
+            PigType(name="Thor", description="Vigueur à la naissance", price=38.0)
         ]
         db.session.bulk_save_objects(pig_types)
         db.session.commit()
