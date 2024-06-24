cd78bca0ce341b2f36f6b5a6180eef03d071e36d
Israel-Kindudi <kindudimandiangu@gmail.com>
2024-06-24 13:45:42 +0100

    mobiusserver launching

--- app.py
+++ app.py
@@ -154,13 +154,3 @@ def admin_dashboard():
         return redirect(url_for('index'))
     return render_template('admin.html')
 
-if __name__ == '__main__':
-   # with app.app_context():
-    #    db.create_all()
-        # Create an admin user (run this once)
-     #   if not User.query.filter_by(username='admin').first():
-     #       admin_user = User(username='admin', email='admin@example.com')
-     #       admin_user.set_password('adminpass')
-     #       db.session.add(admin_user)
-     #       db.session.commit()
-    app.run(debug=True)
